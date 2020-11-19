81a9015f0287357e3b219c496bfcf843e1f8097a
PavelTokarevOfficial <64586673+PavelTokarevOfficial@users.noreply.github.com>
2020-11-19 15:01:52 +0300

    Add files via upload

---
+++ PR6spp/Zad5.cpp
@@ -0,0 +1,47 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "russian");
+    int mass[10], min = 1000, min_i, max = 0, max_i,swap;
+        //Создание массива
+    for (int i = 0; i <= 9; i++)
+    {
+        mass[i] = rand() % 30 + 1;
+    }
+        //Вывод
+    cout << "Массив: \n";
+    for (int i = 0; i <= 9; i++) {
+        cout << mass[i] << " ";
+    }
+        //определение минимального
+    for (int i = 0; i <= 9; i++) {
+        if (mass[i] < min)
+        {
+            min = mass[i];
+            min_i = i;
+        }
+    }
+        //определение массимального
+    for (int i = 0; i <= 9; i++) {
+        if (mass[i] > max)
+        {
+            max = mass[i];
+            max_i = i;
+        }
+    }
+        //swap    
+    swap = mass[min_i];
+    mass[min_i] = mass[max_i];
+    mass[max_i] = swap;
+        //Вывод
+    cout << "\n Массив: \n";
+    for (int i = 0; i <= 9; i++) {
+        cout << mass[i] << " ";
+    }
+}
+
+
